12717d81cd65d57591424dabd3ddd3823ca7bb78
Duncan Roe <duncan_roe@optusnet.com.au>
2019-05-11 18:19:52 +1000

    Update bin/runindent and etc_profile_d/runindent_typedefs_q.sh for new q types

--- etc_profile_d/runindent_typedefs_q.sh
+++ etc_profile_d/runindent_typedefs_q.sh
@@ -9,12 +9,16 @@
 RUNINDENT_TYPEDEFS='
 -TFILE
 -Tbool
+-Tclock_t
 -Tchainbase
 -Tmacro5
 -Tscrbuf5
 -Tsiginfo_t
 -Tsize_t
+-Tuint16_t
 -Tuint32_t
+-Taction
+-Tuint8_t
 '
 
 # Get rid of newline characters
