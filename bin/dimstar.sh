0471dd64f92ddbd047e3e077e4320881b7409a10
Duncan Roe <duncan_roe@optusnet.com.au>
2022-11-06 14:24:13 +1100

    Revamp remote xterm system
    Updated:
    
     bin/README: Updated
    
     bin/dimstar.sh: - Target system which is command basename
                     - Use ssh
                     - Rely on dxt to supply background colour
    
     bin/dxt: Like xt but xterms have a different background (to alert that they
              are running from a remote system)
    
     bin/xt: Implement dxt

--- bin/dimstar.sh
+++ bin/dimstar.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 # \
+export dimstar=$(basename $0 .sh); \
+export localnet=$(cat /etc/HOSTNAME|cut -d. -f2-); \
 exec expect -f "$0" -- "$@"
 
 log_user 1
 #exp_internal 1
-spawn rsh dimstar
+spawn ssh $env(dimstar)
 expect {$ }
 exp_send "xauth\r"
 expect "xauth> "
 exp_send "list\r"
-expect -re "dimstar.local.net:0(\[^\r\]*)\r"
+expect -re "$env(dimstar).$env(localnet):0(\[^\r\]*)\r"
 set i $expect_out(1,string)
 expect "xauth> "
 exp_send "q\r"
@@ -21,13 +23,13 @@ spawn sh
 expect {$ }
 exp_send "xauth\r"
 expect "xauth> "
-exp_send "remove dimstar.local.net:0\r"
+exp_send "remove $env(dimstar).$env(localnet):0\r"
 expect "xauth> "
-exp_send " add dimstar.local.net:0$i\r"
+exp_send " add $env(dimstar).$env(localnet):0$i\r"
 expect "xauth> "
 exp_send "exit\r"
 expect {$ }
-exp_send "xt - -bg palegreen1 -display dimstar.local.net:0\r"
+exp_send "dxt 88x98+0+0 -display $env(dimstar).$env(localnet):0\r"
 expect {$ }
 puts ""
 exit
