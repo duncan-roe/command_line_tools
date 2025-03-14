62ea276ad4bd33609b4378c46933a2ccfc838ba8
Duncan Roe <duncan_roe@optusnet.com.au>
2025-03-14 18:41:23 +1100

    bin/dimstar.sh: don't leave commands in history

--- bin/dimstar.sh
+++ bin/dimstar.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # \
 export dimstar=$(basename $0 .sh); \
-export localnet=$(cat /etc/HOSTNAME|cut -d. -f2-); \
+export localnet=local.net; \
+export HISTFILE=; \
 exec expect -f "$0" -- "$@"
 
 log_user 1
@@ -29,7 +30,8 @@ exp_send " add $env(dimstar).$env(localnet):0$i\r"
 expect "xauth> "
 exp_send "exit\r"
 expect {$ }
-exp_send "dxt 88x98+0+0 -display $env(dimstar).$env(localnet):0\r"
+exp_send "HISTFILE=~/.bash_history dxt 88x98+0+0 \
+  -display $env(dimstar).$env(localnet):0\r"
 expect {$ }
 puts ""
 exit
